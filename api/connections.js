5f705aff89912ebe03096ade86faa1563da94e27
Kendra Lyndon <kendralyndon@gmail.com>
2016-09-08 19:34:53 -0600

    add some connections routes

--- api/connections.js
+++ api/connections.js
@@ -4,9 +4,30 @@ var knex = require('../db/knex');
 var queries = require('../queries/apiQueries');
 module.exports = router;
 
-router.get('/', function(req, res, next){
-  console.log("*******WIN****");
-  queries.test().then(function(data){
-    res.json({test:'hello'});
-      });
+function Connections(){
+  return knex('connections');
+}
+
+router.post('/', function(req, res, next){
+  var input = req.body;
+  input.user_id = //***GET JSON WEB TOKEN***
+  Connections().createConnection(input).then(function(data){
+    res.json(data);
+  })
+});
+
+router.get('/:id', function(req, res, next){
+  Connections().getOneConnection(req.params.id).then(function(connection){
+    res.json(connection);
+  })
+});
+
+router.post('/:id', function(req, res, next){
+  var input = req.body;
+  input.user_id = //***GET JSON WEB TOKEN***
+  Connections().editConnection(input);
+});
+
+router.get('/:id/delete', function(req, res, next){
+  Connections().deleteConnection(req.params.id);
 });
